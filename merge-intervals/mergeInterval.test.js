5092b852e42659c30fdc578391afb1024069b8a4
Fang Liu <fangyiliu@gmail.com>
2021-09-04 14:59:43 -0700

    Autoformat

--- merge-intervals/mergeInterval.test.js
+++ merge-intervals/mergeInterval.test.js
@@ -1,87 +1,121 @@
-const Mocha = require('mocha')
-const assert = require('assert')
-const mocha = new Mocha()
+const Mocha = require('mocha');
+const assert = require('assert');
+const mocha = new Mocha();
 
 mocha.suite.emit('pre-require', this, 'solution', mocha)
 
 describe('Test suite', function() {
   it('works for one interval', () => {
-    const input = [[1,3]]
-    const output = [[1,3]]
-    assert.deepEqual(output, testFunction(input))
-  })
+    const input = [[1, 3]];
+    const output = [[1, 3]];
+    assert.deepEqual(output, testFunction(input));
+  });
   it('works for two non-overlapping intervals', () => {
-    const input = [[1,3], [5,7]];
-    const output = [[1,3], [5,7]];
-    assert.deepEqual(output, testFunction(input))
-  })
+    const input = [
+      [1, 3],
+      [5, 7],
+    ];
+    const output = [
+      [1, 3],
+      [5, 7],
+    ];
+    assert.deepEqual(output, testFunction(input));
+  });
   it('merges two overlapping intervals', () => {
-    const input = [[1,3], [2,4]]
-    const output = [[1,4]]
-    assert.deepEqual(output, testFunction(input))
-  })
+    const input = [
+      [1, 3],
+      [2, 4],
+    ];
+    const output = [[1, 4]];
+    assert.deepEqual(output, testFunction(input));
+  });
   it('merges three overlapping intervals', () => {
-    const input = [[1,3], [2,4], [3, 6]]
-    const output = [[1,6]]
-    assert.deepEqual(output, testFunction(input))
-  })
+    const input = [
+      [1, 3],
+      [2, 4],
+      [3, 6],
+    ];
+    const output = [[1, 6]];
+    assert.deepEqual(output, testFunction(input));
+  });
   it('solves example 1', () => {
-    const input = [[1,3], [2,6], [8, 10], [15,18]]
-    const output = [[1,6], [8,10], [15,18]]
-    assert.deepEqual(output, testFunction(input))
-  })
+    const input = [
+      [1, 3],
+      [2, 6],
+      [8, 10],
+      [15, 18],
+    ];
+    const output = [
+      [1, 6],
+      [8, 10],
+      [15, 18],
+    ];
+    assert.deepEqual(output, testFunction(input));
+  });
   it('solves example 2', () => {
-    const input = [[1,4], [4,5]]
-    const output = [[1,5]]
-    assert.deepEqual(output, testFunction(input))
-  })
+    const input = [
+      [1, 4],
+      [4, 5],
+    ];
+    const output = [[1, 5]];
+    assert.deepEqual(output, testFunction(input));
+  });
   it('solves unsorted intervals', () => {
-    const input = [[4,5], [1,4]]
-    const output = [[1,5]]
-    assert.deepEqual(output, testFunction(input))
-  })
+    const input = [
+      [4, 5],
+      [1, 4],
+    ];
+    const output = [[1, 5]];
+    assert.deepEqual(output, testFunction(input));
+  });
   it('merges same intervals', () => {
-    const input = [[1,4], [1,4]]
-    const output = [[1,4]]
-    assert.deepEqual(output, testFunction(input))
-  })
+    const input = [
+      [1, 4],
+      [1, 4],
+    ];
+    const output = [[1, 4]];
+    assert.deepEqual(output, testFunction(input));
+  });
   it('merges inclusive intervals', () => {
-    const input = [[1,4], [2,3]]
-    const output = [[1,4]]
-    assert.deepEqual(output, testFunction(input))
-  })
-})
+    const input = [
+      [1, 4],
+      [2, 3],
+    ];
+    const output = [[1, 4]];
+    assert.deepEqual(output, testFunction(input));
+  });
+});
 
-mocha.run()
+mocha.run();
 
 const merge = (input) => {
-  let output = []
-  input.sort((a, b) => (a[0] - b[0]))
+  let output = [];
+  input.sort((a, b) => a[0] - b[0]);
   for (let interval of input) {
-    let hasOverlap = false
-    for(let i = 0; i < output.length; i++) {
-      if(overlap(output[i], interval)) {
-        const newInterval = mergeTwo(output[i], interval)
-        replaceInterval(output, i, newInterval)
-        hasOverlap = true
-        break
+    let hasOverlap = false;
+    for (let i = 0; i < output.length; i++) {
+      if (overlap(output[i], interval)) {
+        const newInterval = mergeTwo(output[i], interval);
+        replaceInterval(output, i, newInterval);
+        hasOverlap = true;
+        break;
       }
     }
-    if(!hasOverlap) output.push(interval)
+    if (!hasOverlap) output.push(interval);
   }
-  return output
-}
+  return output;
+};
 
 const overlap = (left, right) => {
-  return (left[0] <= right[0] && left[1] >= right[0])
-}
+  return left[0] <= right[0] && left[1] >= right[0];
+};
 
 const mergeTwo = (left, right) => {
-  return [Math.min(left[0], right[0]), Math.max(left[1], right[1])]
-}
+  return [Math.min(left[0], right[0]), Math.max(left[1], right[1])];
+};
 
 const replaceInterval = (array, index, interval) => {
-  array.splice(index, 1, interval)
-}
+  array.splice(index, 1, interval);
+};
 
 const testFunction = merge;
