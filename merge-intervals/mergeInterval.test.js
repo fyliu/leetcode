5be846615c7f8dc7d3f886e312867a281b54cde8
Fang Liu <fangyiliu@gmail.com>
2021-09-04 14:51:16 -0700

    Make test function name generic

--- merge-intervals/mergeInterval.test.js
+++ merge-intervals/mergeInterval.test.js
@@ -8,53 +8,53 @@ describe('Test suite', function() {
   it('works for one interval', () => {
     const input = [[1,3]]
     const output = [[1,3]]
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
   it('works for two non-overlapping intervals', () => {
     const input = [[1,3], [5,7]];
     const output = [[1,3], [5,7]];
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
   it('merges two overlapping intervals', () => {
     const input = [[1,3], [2,4]]
     const output = [[1,4]]
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
   it('merges three overlapping intervals', () => {
     const input = [[1,3], [2,4], [3, 6]]
     const output = [[1,6]]
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
   it('solves example 1', () => {
     const input = [[1,3], [2,6], [8, 10], [15,18]]
     const output = [[1,6], [8,10], [15,18]]
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
   it('solves example 2', () => {
     const input = [[1,4], [4,5]]
     const output = [[1,5]]
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
   it('solves unsorted intervals', () => {
     const input = [[4,5], [1,4]]
     const output = [[1,5]]
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
   it('merges same intervals', () => {
     const input = [[1,4], [1,4]]
     const output = [[1,4]]
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
   it('merges inclusive intervals', () => {
     const input = [[1,4], [2,3]]
     const output = [[1,4]]
-    assert.deepEqual(output, merge(input))
+    assert.deepEqual(output, testFunction(input))
   })
 })
 
 mocha.run()
 
-const merge= (input) => {
+const merge = (input) => {
   let output = []
   input.sort((a, b) => (a[0] - b[0]))
   for (let interval of input) {
@@ -83,3 +83,5 @@ const mergeTwo = (left, right) => {
 const replaceInterval = (array, index, interval) => {
   array.splice(index, 1, interval)
 }
+
+const testFunction = merge;
