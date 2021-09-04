0ea7a5df92a48fd17f8bf8ec05acd3acfad3462e
Fang Liu <fangyiliu@gmail.com>
2021-09-03 21:26:42 -0700

    set up test

---
+++ merge-intervals/mergeInterval.test.js
@@ -0,0 +1,21 @@
+const Mocha = require('mocha')
+const assert = require('assert')
+const mocha = new Mocha()
+
+mocha.suite.emit('pre-require', this, 'solution', mocha)
+
+describe('Test suite', function() {
+  it('works for one interval', () => {
+    const input = [[1,3]]
+    const output = [[1,3]]
+    assert.deepEqual(output, mergeIntervals(input))
+  })
+})
+
+mocha.run()
+
+
+const mergeIntervals = (input) => {
+  return [[]];
+}
+
