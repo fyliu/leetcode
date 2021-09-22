797413ba804be9f4e4d35ce5e4fd2301d5a0d755
Fang Liu <fangyiliu@gmail.com>
2021-09-21 19:40:41 -0700

    Add alternative solution

--- 1929-concatendation-of-array/exercise.js
+++ 1929-concatendation-of-array/exercise.js
@@ -1,11 +1,19 @@
 var getConcatenation = function(nums) {
-  let ans = []
-  const nums_length = nums.length
+  /**
+   * loop solution
+   */
+  // let ans = []
+  // const nums_length = nums.length
 
-  for(let i = 0; i < 2 * nums_length; i++) {
-    ans.push(nums[i % nums_length])
-  }
-  return ans
+  // for(let i = 0; i < 2 * nums_length; i++) {
+  //   ans.push(nums[i % nums_length])
+  // }
+  // return ans
+
+  /**
+   * js language solution
+   */
+  return nums.concat(nums)
 };
 
 module.exports = getConcatenation
