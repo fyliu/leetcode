4717246ab6b25237161f68540c7fe0f3a0d6e897
Fang Liu <fangyiliu@gmail.com>
2021-09-21 18:31:31 -0700

    Attempt in-place solution
    This is a dead end. A different strategy is needed.

--- build-array-from-permutation/exercise.js
+++ build-array-from-permutation/exercise.js
@@ -1,15 +1,40 @@
+const swap = (list, i, j) => {
+  [list[i], list[j]] = [list[j], list[i]]
+}
+
+const doSwaps = (list, startIndex) => {
+  let currIndex = startIndex //0
+  let endCondition = false
+  let nextIndex = list[currIndex] //5
+
+  while(!endCondition) {
+    const nextSavedIndex = list[nextIndex] //4 3
+    swap(list, currIndex, nextIndex)
+    currIndex = nextIndex //5 4
+    nextIndex = nextSavedIndex //4 3
+
+    endCondition = nextIndex === startIndex
+  }
+}
+
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var buildArray = function(nums) {
-  let result = []
 
-  for (let i = 0; i < nums.length; i++) {
-    result[i] = nums[nums[i]]
+  // for (let i = 0; i < nums.length; i++) {
+  //   result[i] = nums[nums[i]]
+  // }
+
+  let index = 0
+  while(index < nums.length) {
+    const nextIndex = nums[index] + 1
+    doSwaps(nums, index)
+    index = nextIndex
   }
 
-  return result
+  return nums
 };
 
 module.exports = buildArray
--- build-array-from-permutation/exercise.test.js
+++ build-array-from-permutation/exercise.test.js
@@ -5,6 +5,16 @@ describe('Test suite', () => {
   it('example 1', () => {
     const input = [0,2,1,5,3,4];
     const output = [0,1,2,4,5,3];
-    assert.deepStrictEqual(output, testFunction(input));
+    assert.deepStrictEqual(testFunction(input), output);
+  });
+  it('example 2', () => {
+    const input = [5,0,1,2,3,4];
+    const output = [4,5,0,1,2,3];
+    assert.deepStrictEqual(testFunction(input), output);
+  });
+  it.only('example 3', () => {
+    const input = [3,2,1,0];
+    const output = [0,1,2,3];
+    assert.deepStrictEqual(testFunction(input), output);
   });
 });
