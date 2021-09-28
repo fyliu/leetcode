b67f993f72b9854323f83435a2d52571ef8ae02f
Fang Liu <fangyiliu@gmail.com>
2021-09-28 11:12:21 -0700

    Remove dead code

--- 12-integer-to-roman/exercise.js
+++ 12-integer-to-roman/exercise.js
@@ -26,19 +26,8 @@ const romanDigit = (digit, romanOneFiveTen) => {
  * @return {string}
  */
 var intToRoman = function(num) {
-  if(num < 100) {
-    return romanDigit(Math.floor(num / 10), "XLC") + romanDigit(num % 10, "IVX")
-  } else if(num < 1000) {
+  if(num < 1000) {
     return romanDigit(Math.floor(num / 100), "CDM") + romanDigit(Math.floor((num % 100) / 10), "XLC") + romanDigit(num % 10, "IVX")
-
-  } else if(num < 400) {
-    return repeatChar("C", Math.floor(num / 100)) + intToRoman(num % 100)
-  } else if(num < 500) {
-    return "CD" + intToRoman(num - 400)
-  } else if(num < 900) {
-    return "D" + intToRoman(num - 500)
-  } else if(num < 1000) {
-    return "CM" + intToRoman(num - 900)
 
   } else {
     return repeatChar("M", Math.floor(num / 1000)) + intToRoman(num % 1000)
